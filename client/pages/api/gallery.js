2a81038ab8e2cb2d230bf71ded1ca923c2332902
Alvin Rumbaoa <alvinrumbaoa@gmail.com>
2021-12-09 07:54:29 -0800

    CHANGES

---
+++ client/pages/api/gallery.js
@@ -0,0 +1,18 @@
+import React, {useEffect , Suspense} from 'react'
+import Head from 'next/head'
+import {Flex, Heading, Text , Image , Box} from '@chakra-ui/react'
+import { Spinner } from '@chakra-ui/react'
+import Nav from '../components/Nav';
+import { Stack, HStack, VStack } from '@chakra-ui/react'
+import { motion } from "framer-motion";
+import Aos from "aos";
+import "aos/dist/aos.css"
+
+export default function gallery() {
+	const src=["","","",""]
+	return (
+		<>
+		<h1>Testing</h1>
+		</>
+	)
+}
