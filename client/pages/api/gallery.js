caf0abdea9b066be7fc938f1bd96d4a7d9b6f21d
Alvin Rumbaoa <alvinrumbaoa@gmail.com>
2021-12-10 08:31:17 -0800

    deploy

--- client/pages/api/gallery.js
+++ client/pages/api/gallery.js
@@ -1,11 +1,14 @@
-import React, {useEffect , Suspense} from 'react'
+import React, {useEffect} from 'react'
 import Head from 'next/head'
-import {Flex, Heading, Text , Image , Box} from '@chakra-ui/react'
+import Link from 'next/link'
+import {Flex, Heading, Text , Grid ,Image , Box} from '@chakra-ui/react'
 import { Spinner } from '@chakra-ui/react'
-import { Stack, HStack, VStack, Grid } from '@chakra-ui/react'
+import { Stack, HStack, VStack } from '@chakra-ui/react'
 import { motion } from "framer-motion";
 import Aos from "aos";
 import "aos/dist/aos.css"
+const MotionFlex = motion(Flex)
+React.useLayoutEffect = React.useEffect 
 
 export default function gallery() {
 	const src=["./images/grid1.jpg",
@@ -14,15 +17,23 @@ export default function gallery() {
 				"/images/grid4.jpg",
 			"","","","","",""]
 
+			const onCloseHandler = () =>{
+				<Link href="/home"/>
+			}  
 
 	return (
 		<Flex justifyContent="center" align="center">
 			<Grid templateColumns='repeat(3, 1fr)' gap={6}> 
 			{
 							src.map((image, idx)=>(
-								<motion.div  whileHover={{ scale: 1.1 }}  whileTap={{ scale: 0.9 }} >
+								<MotionFlex  whileHover={{ scale: 1.1 }}  whileTap={{ scale: 0.9 }} >
+									<Link href={"/images/" + image.src}>
 									<Image src={image} alt="event images"/>
-								</motion.div>
+
+									</Link>
+									
+
+								</MotionFlex>
 							))
 			}
 			</Grid>
